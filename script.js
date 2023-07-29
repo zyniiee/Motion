f0bf2b348014d1eb584a64962f8a3274ee36b68b
Zyn <zyniiee@gmail.com>
2023-07-29 15:36:40 +1200

    first commit

---
+++ script.js
@@ -0,0 +1,109 @@
+window.addEventListener("beforeunload", function () {
+  document.body.classList.add("animate-out");
+});
+
+let menuClick = document.querySelector(".menu_line-cover");
+let menuHidden = document.querySelector(".st_effect");
+
+function addClass() {
+  menuClick.classList.toggle("open");
+  menuHidden.classList.toggle("st_menu-open");
+}
+
+function closeMenu() {
+  menuHidden?.classList.remove("st_menu-open");
+  menuClick?.classList.remove("open");
+}
+menuClick.addEventListener("click", addClass);
+menuHidden.addEventListener("click", closeMenu);
+
+const cursor = document.querySelector("#cursor");
+const cursorCircle = cursor.querySelector(".cursor__circle");
+
+const mouse = { x: -100, y: -100 }; // mouse pointer's coordinates
+const pos = { x: 0, y: 0 }; // cursor's coordinates
+const speed = 0.1; // between 0 and 1
+
+const updateCoordinates = (e) => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+};
+
+window.addEventListener("mousemove", updateCoordinates);
+
+function getAngle(diffX, diffY) {
+  return (Math.atan2(diffY, diffX) * 180) / Math.PI;
+}
+
+function getSqueeze(diffX, diffY) {
+  const distance = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+  const maxSqueeze = 0.15;
+  const accelerator = 1500;
+  return Math.min(distance / accelerator, maxSqueeze);
+}
+
+const updateCursor = () => {
+  const diffX = Math.round(mouse.x - pos.x);
+  const diffY = Math.round(mouse.y - pos.y);
+
+  pos.x += diffX * speed;
+  pos.y += diffY * speed;
+
+  const angle = getAngle(diffX, diffY);
+  const squeeze = getSqueeze(diffX, diffY);
+
+  const scale = "scale(" + (1 + squeeze) + ", " + (1 - squeeze) + ")";
+  const rotate = "rotate(" + angle + "deg)";
+  const translate = "translate3d(" + pos.x + "px ," + pos.y + "px, 0)";
+
+  cursor.style.transform = translate;
+  cursorCircle.style.transform = rotate + scale;
+};
+
+function loop() {
+  updateCursor();
+  requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
+
+const cursorModifiers = document.querySelectorAll("[cursor-class]");
+
+cursorModifiers.forEach((curosrModifier) => {
+  curosrModifier.addEventListener("mouseenter", function () {
+    const className = this.getAttribute("cursor-class");
+    cursor.classList.add(className);
+  });
+
+  curosrModifier.addEventListener("mouseleave", function () {
+    const className = this.getAttribute("cursor-class");
+    cursor.classList.remove(className);
+  });
+});
+
+const slider = document.querySelector(".horizontal");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = x - startX;
+  slider.scrollLeft = scrollLeft - walk;
+});
